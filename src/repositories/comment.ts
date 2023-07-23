07ce1b195f28e5468d4349fc4c718f93a7f365c7
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-07-24 08:43:15 +0900

    댓글 레포지토리 개선

--- src/repositories/comment.ts
+++ src/repositories/comment.ts
@@ -1,22 +1,30 @@
 import supabase from "../libs/supabase"
-import { Comment, CommentInsertDto, Comments, Profile, Response } from "../libs/supabase/types"
+import { Comment, CommentInsertDto, CommentUpdateDto, CommentsView, Profile, Response } from "../libs/supabase/types"
 
-export type GetCommentsDto = Comment & {
+export type CommentGetDtoWithProfile = Comment & {
 	profiles: Profile
 }
 
 class CommentRepository {
-	public async fetchBySiteAndContent(siteId: number, content_id: string): Promise<Response<GetCommentsDto[]>> {
-		return await supabase.from<'comments', Comments>('comments')
+	public async fetchBySiteAndContent(siteId: number, content_id: string): Promise<Response<CommentGetDtoWithProfile[]>> {
+		return await supabase.from<'comments_without_passwords', CommentsView>('comments_without_passwords')
 				.select('*, profiles(*)')
 				.eq("site_id", siteId)
 				.eq("content_id", content_id)
-				.returns<GetCommentsDto[]>()
+				.returns<CommentGetDtoWithProfile[]>()
 	}
 
-	public async saveComment(comment: CommentInsertDto): Promise<Response<Comment>> {
-		return await supabase.from('comments').insert(comment).select().returns<Comment>()
+	public async save(comment: CommentInsertDto): Promise<Response<null>> {
+		return await supabase.from('comments').insert(comment)
 	}
+
+  public async modify(comment: CommentUpdateDto): Promise<Response<Comment>> {
+    return await supabase.from('comments').update(comment).select().returns<Comment>()
+  }
+
+  public async delete(commentId: number): Promise<Response<null>> {
+    return await supabase.from('comments').delete().eq('id', commentId)
+  }
 }
 
 const commentRepository = new CommentRepository()
