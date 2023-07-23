af694e1545e8ca58aa14d67c9f5d1cc03bfda22c
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-07-24 08:42:31 +0900

    CommentViewer 콘텍스트메뉴 추가 및 스타일 변경

--- src/components/comments/CommentItem/index.tsx
+++ src/components/comments/CommentItem/index.tsx
@@ -1,20 +1,36 @@
+import dayjs from "dayjs"
+import "dayjs/plugin/duration"
+import * as relativeTime from "dayjs/plugin/relativeTime"
 import { Comment, Profile } from "../../../libs/supabase/types"
+import { EllipsisHorizontalIcon } from "@heroicons/react/20/solid"
+import { MouseEvent } from "react"
+
+dayjs.extend(relativeTime)
 
 type CommentItemProps = {
   comment: Comment,
-  profile: Profile
+  profile: Profile|null,
+  userId: string|null,
+  onContextMenuOpen: (commentId: number, e: MouseEvent<SVGSVGElement, globalThis.MouseEvent>) => void,
 }
 
-export default function CommentItem({comment, profile}: CommentItemProps) {
+export default function CommentItem({comment, profile, userId, onContextMenuOpen}: CommentItemProps) {
   return (
-    <div>
-      {profile.avatar_url && <img src={profile.avatar_url} width="30"/>}
-      <div>
-        {profile.user_name}
+    <div className="w-full mb-3 pb-3 px-3 border-b last:mb-0">
+      <div className="flex mb-2">
+        {profile?.avatar_url && <img className="rounded-full border" src={profile.avatar_url} width="30"/>}
+        <div className="ml-2">
+          {profile?.user_name ?? comment.nickname} · <span className="text-stone-700" title={dayjs(comment.created_at).format()}>{dayjs().to(dayjs(comment.created_at))}</span>
+        </div>
+        <div className="flex-1"></div>
+        <div>
+          <EllipsisHorizontalIcon className="h-6 w-6 text-stone-500 cursor-pointer" onClick={e => {onContextMenuOpen(comment.id, e)}}/>
+        </div>
       </div>
       <div>
         {comment.content}
       </div>
     </div>
+
   )
 }
--- src/components/comments/CommentViewer/index.tsx
+++ src/components/comments/CommentViewer/index.tsx
@@ -1,23 +1,69 @@
 import { useQuery } from "@tanstack/react-query"
 import commentRepository from "../../../repositories/comment"
 import CommentItem from "../CommentItem"
+import ContextMenu from "../../ContextMenu"
+import ContextMenuItem from "../../ContextMenu/ContextMenuItem"
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline"
+import { createRef, useRef, useState } from "react"
+import useComponentVisible from "../../../hooks/useComponentVisible"
 
 type CommentViewerProps = {
-  siteId: number,
+  className?: string
+  siteId: number
   contentId: string
+  userId: string|null
 }
 
-export default function CommentViewer({ siteId, contentId }: CommentViewerProps) {
-  const { isLoading, data } = useQuery({
+export default function CommentViewer({ className, siteId, contentId, userId }: CommentViewerProps) {
+  const { isLoading, data, refetch } = useQuery({
     queryKey: ['comments'],
     queryFn: () => commentRepository.fetchBySiteAndContent(siteId, contentId).then(r => r.data)
   })
-  console.log(data)
+
+  const {ref, isComponentVisible, setIsComponentVisible} = useComponentVisible(false)
+  const [workingCommentId, setWorkingCommentId] = useState<number|null>(null)
+  const [top, setTop] = useState(0)
+  const [left, setLeft] = useState(0)
 
   return (
-    <div>
+    <div className={`pt-3 border-t border-x ${className}`}>
+      <ContextMenu
+        ref={ref}
+        className={`${isComponentVisible ? '' : 'invisible'} absolute bg-white`}
+        style={{top: top, left: left}}>
+        <ContextMenuItem
+          id="edit"
+          icon={<PencilSquareIcon className="w-5 h-5"/>}
+          title="편집"
+          onClick={() => {}}/>
+        <ContextMenuItem
+          className="text-red-500"
+          id="delete"
+          icon={<TrashIcon className="w-5 h-5"/>}
+          title="삭제"
+          onClick={() => {
+            if (workingCommentId != null) {
+              const deleteComment = async () => {
+                await commentRepository.delete(workingCommentId)
+                refetch()
+              }
+              deleteComment()
+            }
+          }}/>
+      </ContextMenu>
       {isLoading ? <>로딩중</> : (data ?? []).map((comment) => (
-        <CommentItem key={comment.id} comment={comment} profile={comment.profiles}/>
+        <CommentItem
+          key={comment.id}
+          comment={comment}
+          profile={comment.profiles}
+          userId={userId}
+          onContextMenuOpen={(id, e) => {
+            setWorkingCommentId(id)
+            setIsComponentVisible(true)
+            const rect = e.currentTarget.getBoundingClientRect()
+            setLeft(rect.left - (ref.current?.clientWidth ?? 0))
+            setTop(rect.top + (e.currentTarget.clientHeight / 2))
+          }}/>
       ))}
     </div>
   )
