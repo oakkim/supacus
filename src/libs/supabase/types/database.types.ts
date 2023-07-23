bb173bb38a515878e85e4637f6f7480a5aa2e5b3
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-07-24 08:40:58 +0900

    sync with supabase table and view

--- src/libs/supabase/types/database.types.ts
+++ src/libs/supabase/types/database.types.ts
@@ -16,8 +16,11 @@ export interface Database {
           created_at: string | null
           id: number
           ip: string | null
+          nickname: string | null
           parent_id: number | null
+          password: string | null
           site_id: number
+          updated_at: string
           user_id: string | null
         }
         Insert: {
@@ -26,8 +29,11 @@ export interface Database {
           created_at?: string | null
           id?: number
           ip?: string | null
+          nickname?: string | null
           parent_id?: number | null
+          password?: string | null
           site_id: number
+          updated_at?: string
           user_id?: string | null
         }
         Update: {
@@ -36,8 +42,11 @@ export interface Database {
           created_at?: string | null
           id?: number
           ip?: string | null
+          nickname?: string | null
           parent_id?: number | null
+          password?: string | null
           site_id?: number
+          updated_at?: string
           user_id?: string | null
         }
         Relationships: [
@@ -50,7 +59,7 @@ export interface Database {
           {
             foreignKeyName: "comments_user_id_fkey"
             columns: ["user_id"]
-            referencedRelation: "users"
+            referencedRelation: "profiles"
             referencedColumns: ["id"]
           }
         ]
@@ -106,14 +115,65 @@ export interface Database {
           {
             foreignKeyName: "sites_user_id_fkey"
             columns: ["user_id"]
-            referencedRelation: "users"
+            referencedRelation: "profiles"
             referencedColumns: ["id"]
           }
         ]
       }
     }
     Views: {
-      [_ in never]: never
+      comments_without_passwords: {
+        Row: {
+          content: string | null
+          content_id: string | null
+          created_at: string | null
+          id: number | null
+          ip: string | null
+          nickname: string | null
+          parent_id: number | null
+          site_id: number | null
+          updated_at: string | null
+          user_id: string | null
+        }
+        Insert: {
+          content?: string | null
+          content_id?: string | null
+          created_at?: string | null
+          id?: number | null
+          ip?: string | null
+          nickname?: string | null
+          parent_id?: number | null
+          site_id?: number | null
+          updated_at?: string | null
+          user_id?: string | null
+        }
+        Update: {
+          content?: string | null
+          content_id?: string | null
+          created_at?: string | null
+          id?: number | null
+          ip?: string | null
+          nickname?: string | null
+          parent_id?: number | null
+          site_id?: number | null
+          updated_at?: string | null
+          user_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "comments_site_id_fkey"
+            columns: ["site_id"]
+            referencedRelation: "sites"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "comments_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
     }
     Functions: {
       [_ in never]: never
--- src/libs/supabase/types/index.ts
+++ src/libs/supabase/types/index.ts
@@ -4,25 +4,31 @@ import type { Database } from './database.types'
 
 // -------Utility-------
 export type TableNames = keyof Database['public']['Tables']
+export type ViewNames = keyof Database['public']['Views']
+export type EntityNames = TableNames|ViewNames
 
 export type Table<T extends TableNames> = Database['public']['Tables'][T]
-export type Row<T extends TableNames> = Table<T>['Row']
-export type InsertDto<T extends TableNames> = Table<T>['Insert']
-export type UpdateDto<T extends TableNames> = Table<T>['Update']
+export type View<T extends ViewNames> = Database['public']['Views'][T]
+export type Entity<T extends EntityNames> = T extends TableNames ? Table<T> : T extends ViewNames ? View<T> : never
 
-export type SchemaResponse<T extends Schema<any>> = PostgrestSingleResponse<T['table']['Row'][]>
+export type Row<T extends EntityNames> = Entity<T>['Row']
+export type InsertDto<T extends EntityNames> = Entity<T>['Insert']
+export type UpdateDto<T extends EntityNames> = Entity<T>['Update']
+
+export type SchemaResponse<T extends Schema<any>> = PostgrestSingleResponse<T['entity']['Row'][]>
 export type Response<T> = PostgrestSingleResponse<T>
 export type ResponseData<T> = Response<T>['data']
 export type ResponseError<T> = Response<T>['error']
 
 // -------Schema-------
-export type Schema<T extends TableNames> = {
-    table: Table<T>,
+export type Schema<T extends EntityNames> = {
+    entity: Entity<T>,
     name: T
 }
 
 export type Sites = Schema<'sites'>
 export type Comments = Schema<'comments'>
+export type CommentsView = Schema<'comments_without_passwords'>
 export type Profiles = Schema<'profiles'>
 
 export type Site = Row<'sites'>
@@ -30,6 +36,7 @@ export type SiteInsertDto = InsertDto<'sites'>
 export type SiteUpdateDto = UpdateDto<'sites'>
 
 export type Comment = Row<'comments'>
+export type CommentGetDto = Row<'comments_without_passwords'>
 export type CommentInsertDto = InsertDto<'comments'>
 export type CommentUpdateDto = UpdateDto<'comments'>
 
