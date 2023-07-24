66258fb7217f62c0bc1f39e77aedad5e405785ef
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-07-25 08:46:36 +0900

    feat: 현재 유저의 댓글만 수정 ... 보여주기

--- src/components/comments/CommentItem/index.tsx
+++ src/components/comments/CommentItem/index.tsx
@@ -17,15 +17,18 @@ type CommentItemProps = {
 export default function CommentItem({comment, profile, userId, onContextMenuOpen}: CommentItemProps) {
   return (
     <div className="w-full mb-3 pb-3 px-3 border-b last:mb-0">
-      <div className="flex mb-2">
-        {profile?.avatar_url && <img className="rounded-full border" src={profile.avatar_url} width="30"/>}
-        <div className="ml-2">
+      <div className="flex mb-2 items-center">
+        {profile?.avatar_url && <img className="rounded-full border mr-2" src={profile.avatar_url} width="30"/>}
+        <div>
           {profile?.user_name ?? comment.nickname} · <span className="text-stone-700" title={dayjs(comment.created_at).format()}>{dayjs().to(dayjs(comment.created_at))}</span>
         </div>
         <div className="flex-1"></div>
-        <div>
-          <EllipsisHorizontalIcon className="h-6 w-6 text-stone-500 cursor-pointer" onClick={e => {onContextMenuOpen(comment.id, e)}}/>
-        </div>
+        {
+          userId == profile?.id ? <div>
+            <EllipsisHorizontalIcon className="h-6 w-6 text-stone-500 cursor-pointer" onClick={e => {onContextMenuOpen(comment.id, e)}}/>
+          </div> : <></>
+        }
+
       </div>
       <div>
         {comment.content}
