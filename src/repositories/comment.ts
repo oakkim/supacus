dde90eccf197344bebcadb3a4d699c13b25d4c47
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-08-03 17:00:47 +0900

    fix: tab을 space 2개로 변경

--- src/repositories/comment.ts
+++ src/repositories/comment.ts
@@ -2,21 +2,21 @@ import supabase from "../libs/supabase"
 import { Comment, CommentInsertDto, CommentUpdateDto, CommentsView, Profile, Response } from "../libs/supabase/types"
 
 export type CommentGetDtoWithProfile = Comment & {
-	profiles: Profile
+  profiles: Profile
 }
 
 class CommentRepository {
-	public async fetchBySiteAndContent(siteId: number, content_id: string): Promise<Response<CommentGetDtoWithProfile[]>> {
-		return await supabase.from<'comments_without_passwords', CommentsView>('comments_without_passwords')
-				.select('*, profiles(*)')
-				.eq("site_id", siteId)
-				.eq("content_id", content_id)
-				.returns<CommentGetDtoWithProfile[]>()
-	}
+  public async fetchBySiteAndContent(siteId: number, content_id: string): Promise<Response<CommentGetDtoWithProfile[]>> {
+    return await supabase.from<'comments_without_passwords', CommentsView>('comments_without_passwords')
+        .select('*, profiles(*)')
+        .eq("site_id", siteId)
+        .eq("content_id", content_id)
+        .returns<CommentGetDtoWithProfile[]>()
+  }
 
-	public async save(comment: CommentInsertDto): Promise<Response<null>> {
-		return await supabase.from('comments').insert(comment)
-	}
+  public async save(comment: CommentInsertDto): Promise<Response<null>> {
+    return await supabase.from('comments').insert(comment)
+  }
 
   public async modify(comment: CommentUpdateDto): Promise<Response<Comment>> {
     return await supabase.from('comments').update(comment).select().returns<Comment>()
@@ -26,9 +26,9 @@ class CommentRepository {
     return await supabase.from('comments').delete().eq('id', commentId)
   }
 
-	public async deleteAnonComment(commentId: number, password: string): Promise<Response<null>> {
-		return await supabase.rpc('delete_anon_comment', {id: commentId, password})
-	}
+  public async deleteAnonComment(commentId: number, password: string): Promise<Response<null>> {
+    return await supabase.rpc('delete_anon_comment', {id: commentId, password})
+  }
 }
 
 const commentRepository = new CommentRepository()
