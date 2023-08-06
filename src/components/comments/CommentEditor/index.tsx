17228d488df5c2677144c37fb966903d7a03e3e5
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-08-07 01:09:54 +0900

    fix:  textarea에서 한국어 입력 후 엔터 누르면 두번 호출되어 두번 등록되던 문제 해결

--- src/components/comments/CommentEditor/index.tsx
+++ src/components/comments/CommentEditor/index.tsx
@@ -64,7 +64,9 @@ export default function CommentEditor({ className, userId, profile, siteId, cont
             disabled={!allowAnonymous && userId == null}
             onChange={e => setContent(e.target.value)}
             onKeyDown={e => {
-              if (e.key == 'Enter') {
+              // 한국어는 함수 호출이 두번되는 문제
+              // https://1two13.tistory.com/entry/react-onKeyDown-%EC%9D%B4%EB%B2%A4%ED%8A%B8-%ED%95%9C%EA%B8%80-%EC%9E%85%EB%A0%A5-%EC%8B%9C-%ED%95%A8%EC%88%98%EA%B0%80-%EB%91%90-%EB%B2%88-%EC%8B%A4%ED%96%89%EB%90%98%EB%8A%94-%EB%AC%B8%EC%A0%9C-%ED%95%B4%EA%B2%B0%ED%95%98%EA%B8%B0#:~:text=%EA%B2%80%EC%83%89%EC%B0%BD%EC%97%90%20%EA%B2%80%EC%83%89%EC%96%B4%EB%A5%BC%20%EC%9E%85%EB%A0%A5%ED%95%98%EA%B3%A0%20%ED%82%A4%EB%B3%B4%EB%93%9C%EB%A5%BC%20%EC%82%AC%EC%9A%A9%ED%95%98%EC%97%AC%20%EC%9C%84%EC%95%84%EB%9E%98%EC%97%90%20%EC%9D%B4%EB%8F%99%20%EC%8B%9C%2C,%EC%A1%B0%ED%95%A9%EB%AC%B8%EC%9E%90%EC%9D%B8%EC%A7%80%20%EC%95%84%EB%8B%8C%EC%A7%80%EB%A5%BC%20%ED%8C%90%EB%8B%A8%ED%95%9C%EB%8B%A4.%20%ED%95%9C%EA%B8%80%EC%9D%80%20%EC%A1%B0%ED%95%A9%EB%AC%B8%EC%9E%90%EC%9D%B4%EA%B3%A0%2C%20%EC%98%81%EC%96%B4%EB%8A%94%20%EC%A1%B0%ED%95%A9%EB%AC%B8%EC%9E%90%EA%B0%80%20%EC%95%84%EB%8B%88%EB%8B%A4.
+              if (e.key == 'Enter' && e.nativeEvent.isComposing == false) {
                 saveComment();
                 e.preventDefault();
               }
