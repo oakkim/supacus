8f75e5faf221fa9645377dfc8f2a19d8d1106654
Kim Daeyong <dev.kimdaeyong@gmail.com>
2023-08-14 01:11:30 +0900

    feat: 로그아웃 버튼 위치 변경, 프로필 사진 위치 변경, 단축키 기능 추가

--- src/components/comments/CommentEditor/index.tsx
+++ src/components/comments/CommentEditor/index.tsx
@@ -3,13 +3,12 @@ import commentRepository from "../../../repositories/comment"
 import { CommentInsertDto, Profile } from "../../../libs/supabase/types"
 import kakaoLogo from '../../../assets/auth/icons/icon_kakao.svg'
 import supabase from "../../../libs/supabase"
-import ContextMenu from "../../ContextMenu"
-import ContextMenuItem from "../../ContextMenu/ContextMenuItem"
 import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/outline"
-import useComponentVisible from "../../../hooks/useComponentVisible"
 import { useDispatch } from "react-redux"
 import { userAction } from "../../../stores/user"
 import useClientIp from "../../../hooks/useClientIp"
+import useShortcut from "../../../hooks/useShortcut"
+import { useMediaQuery } from "react-responsive"
 
 type CommentEditorProps = {
   className?: string,
@@ -25,10 +24,6 @@ export default function CommentEditor({ className, userId, profile, siteId, cont
   const nicknameInputRef = useRef<HTMLInputElement>(null)
   const [nickname, setNickname] = useState<string>("")
   const [content, setContent] = useState<string>("")
-
-  const {ref, componentVisible, setComponentVisible} = useComponentVisible(false)
-  const [left, setLeft] = useState<number>(0)
-  const [top, setTop] = useState<number>(0)
 
   const dispatch = useDispatch()
   const ip = useClientIp()
@@ -61,82 +56,92 @@ export default function CommentEditor({ className, userId, profile, siteId, cont
     }
     insert()
   }, [nickname, content, contentId, siteId, userId, onSubmit])
+
+  const handleSaveCommentAction = useCallback((e: KeyboardEvent) => {
+    saveComment()
+    e.preventDefault()
+  }, [saveComment])
+
+  const handleNewLineAction = useCallback((e: KeyboardEvent) => {
+    setContent(content + "\n")
+    e.preventDefault()
+  }, [content, setContent])
   
+  const isMobile = useMediaQuery({maxWidth: '500px'})
+  const ref = useShortcut<HTMLTextAreaElement>([{
+    keys: ['Enter'],
+    callback: handleSaveCommentAction,
+    disabled: isMobile
+  }, {
+    keys: ['Shift', 'Enter'],
+    callback: handleNewLineAction,
+    disabled: isMobile
+  }])
+
   return (
     <>
       <div className={`flex flex-col p-3 border ${className}`}>
-        <div className="flex flex-col">
-          <textarea
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-start">
+            {
+              !!userId ? <div className="flex flex-row items-center">
+                {profile?.avatar_url && <img className="rounded-full border mr-2 w-8 h-8" src={profile.avatar_url}/>}
+                <div>
+                  {profile?.user_name}
+                </div>
+              </div> : <>
+                {allowAnonymous ? 
+                  <>
+                    <input ref={nicknameInputRef}
+                      type="text"
+                      name="nickname"
+                      placeholder="닉네임"
+                      className="border rounded-md pl-1.5 py-1.5 mr-2 text-sm w-24"
+                      value={nickname}
+                      onChange={(e) => setNickname(e.target.value)}/>
+                  </>
+                  :
+                  <></>
+                }
+                <div className="flex items-center">
+                  <div className="flex items-center justify-center bg-kakao p-2 rounded-md cursor-pointer w-8 h-8" onClick={() => {
+                      window.open('/login', 'Login', 'width=500, height=800, location=no, toolbars=no')
+                    }}>
+                    <img className="w-full" src={kakaoLogo}/>
+                  </div>
+                </div>
+              </>
+            }
+            <div className="flex-1"></div>
+            <div className={`${profile != null ? "visible" : "invisible"} flex flex-row items-center justify-center cursor-pointer gap-1 py-1`} onClick={() => {
+              const logout = async () => {
+                await supabase.auth.signOut()
+                dispatch(userAction.reset())
+              }
+              logout()
+            }}>
+              <ArrowLeftOnRectangleIcon className="w-5"/>
+              <div>로그아웃</div>
+            </div> 
+          </div>
+          <textarea ref={ref}
             name="content"
             className="border rounded-md flex-1 pl-3 pt-3 min-h-[6rem]"
             value={content}
             placeholder={!allowAnonymous && userId == null ? "로그인 후 댓글을 작성해보세요." : ""}
             disabled={!allowAnonymous && userId == null}
-            onChange={e => setContent(e.target.value)}
-            onKeyDown={e => {
-              // 한국어는 함수 호출이 두번되는 문제
-              // https://1two13.tistory.com/entry/react-onKeyDown-%EC%9D%B4%EB%B2%A4%ED%8A%B8-%ED%95%9C%EA%B8%80-%EC%9E%85%EB%A0%A5-%EC%8B%9C-%ED%95%A8%EC%88%98%EA%B0%80-%EB%91%90-%EB%B2%88-%EC%8B%A4%ED%96%89%EB%90%98%EB%8A%94-%EB%AC%B8%EC%A0%9C-%ED%95%B4%EA%B2%B0%ED%95%98%EA%B8%B0#:~:text=%EA%B2%80%EC%83%89%EC%B0%BD%EC%97%90%20%EA%B2%80%EC%83%89%EC%96%B4%EB%A5%BC%20%EC%9E%85%EB%A0%A5%ED%95%98%EA%B3%A0%20%ED%82%A4%EB%B3%B4%EB%93%9C%EB%A5%BC%20%EC%82%AC%EC%9A%A9%ED%95%98%EC%97%AC%20%EC%9C%84%EC%95%84%EB%9E%98%EC%97%90%20%EC%9D%B4%EB%8F%99%20%EC%8B%9C%2C,%EC%A1%B0%ED%95%A9%EB%AC%B8%EC%9E%90%EC%9D%B8%EC%A7%80%20%EC%95%84%EB%8B%8C%EC%A7%80%EB%A5%BC%20%ED%8C%90%EB%8B%A8%ED%95%9C%EB%8B%A4.%20%ED%95%9C%EA%B8%80%EC%9D%80%20%EC%A1%B0%ED%95%A9%EB%AC%B8%EC%9E%90%EC%9D%B4%EA%B3%A0%2C%20%EC%98%81%EC%96%B4%EB%8A%94%20%EC%A1%B0%ED%95%A9%EB%AC%B8%EC%9E%90%EA%B0%80%20%EC%95%84%EB%8B%88%EB%8B%A4.
-              if (e.key == 'Enter' && e.nativeEvent.isComposing == false) {
-                saveComment();
-                e.preventDefault();
-              }
-            }}/> 
-          <div className="flex mt-2">
-            <div className="flex mb-2 justify-start">
-              {
-                !!userId ? <div className="flex flex-row items-center cursor-pointer" onClick={(e) => {
-                      setComponentVisible(true)
-                      const rect = e.currentTarget.getBoundingClientRect()
-                      setLeft(rect.left + window.scrollX)
-                      setTop(rect.top + (e.currentTarget.clientHeight + 5) + window.scrollY)
-                    }}>
-                  {profile?.avatar_url && <img className="rounded-full border mr-2" src={profile.avatar_url} width="30"/>}
-                  <div>
-                    {profile?.user_name}
-                  </div>
-                </div> : <>
-                  {allowAnonymous ? 
-                    <>
-                      <input ref={nicknameInputRef}
-                        type="text"
-                        name="nickname"
-                        placeholder="닉네임"
-                        className="border rounded-md pl-1.5 py-1.5 mr-2 text-sm w-24"
-                        value={nickname}
-                        onChange={(e) => setNickname(e.target.value)}/>
-                    </>
-                    :
-                    <></>
-                  }
-                  <div className="flex items-center">
-                    <div className="flex items-center justify-center bg-kakao p-2 rounded-md cursor-pointer w-8 h-8" onClick={() => {
-                        window.open('/login', 'Login', 'width=500, height=800, location=no, toolbars=no')
-                      }}>
-                      <img className="w-full" src={kakaoLogo}/>
-                    </div>
-                  </div>
-                </>
-              }
+            onChange={e => setContent(e.target.value)}/> 
+          <div className="flex items-center">
+            <div>
+              {isMobile ? '' : 'Shift+Enter로 줄바꿈'}
             </div>
             <div className="flex-1"/>
-            <button className="h-[fit-content]" onClick={saveComment}>작성</button>
+            <div>
+              <button className="h-full p-0 px-4 py-1.5" onClick={saveComment}>작성</button>
+            </div>
           </div>
         </div>
       </div>
-      <ContextMenu ref={ref} className={`${componentVisible ? '' : 'invisible'} absolute bg-white`} style={{left, top}}>
-        <ContextMenuItem
-          id="logout"
-          title="로그아웃"
-          icon={<ArrowLeftOnRectangleIcon className="w-5"/>}
-          onClick={() => {
-            const logout = async () => {
-              await supabase.auth.signOut()
-              dispatch(userAction.reset())
-            }
-            logout()
-            setComponentVisible(false)
-          }}/>
-      </ContextMenu>
     </>
 )
 }
